c010b8ad253091a318fbbf7a2b024ac260252e4a
Kasimbudeir <qasem.bdier@powercoders.org>
2019-04-01 23:07:02 +0300

    modify the code to call the function from javascript not from html

--- js problem/setClass.js
+++ js problem/setClass.js
@@ -1,5 +1,5 @@
 console.log('My code is runing');
-
+let element = document.getElementById('mypara');
 let greet = function (name) {
     console.log('Hello ' + name);
 }
@@ -18,3 +18,25 @@ function clearClass() {
     let element = document.getElementById('mypara');
     element.className = '';
 }
+
+
+
+function setClass(event,className) {
+    console.log('the function is work');
+    element.className = className;
+}
+let li1 = document.getElementById('li1');
+console.log(li1);
+li1.addEventListener("click" ,function(){
+    setClass(event,'warning');
+});
+
+let li2 = document.getElementById('li2');
+li2.addEventListener('click', function (){
+    setClass(event,'tip')
+}); 
+
+let li3 = document.getElementById('li3');
+li3.addEventListener('click', function (){
+    setClass(event,'')
+}); 
