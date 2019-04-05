2aa03e6f0038dc5f9bdf901872e311e9dacbea0e
Kasimbudeir <qasem.bdier@powercoders.org>
2019-04-05 11:57:00 +0300

    prevent user adding empty element by fixing white space

--- Shopping/shopping.js
+++ Shopping/shopping.js
@@ -1,52 +1,57 @@
-document.querySelector('button').addEventListener('click', function (event) {
-    let inputBox = document.getElementById('item');
-    //to start typping after adding item in input element
-    inputBox.focus();
-    if (inputBox.value !==""){
-        let myShoppingList = document.getElementById('my-shoppingList');
-        myShoppingList.appendChild(createNewListItem(inputBox.value));  
-    }
-    else{
 
-alert('It is empty value');
+let inputBox = document.getElementById('item');
+let myShoppingList = document.getElementById('my-shoppingList');
+
+document.querySelector('button').addEventListener('click', function (event) {
+    //to start typping after adding item in input element
+    inputBox.value = inputBox.value.trim();
+    if (inputBox.value) {
+        myShoppingList.appendChild(createNewListItem(inputBox.value));
+    
     }
-   
+    else {
+        
+
+        alert('It is empty value');
+    }
+
     inputBox.value = '';
-    
-    
+    inputBox.focus();
+
 });
 // start typping in this element
-document.getElementById('item').focus();
+inputBox.focus();
 
 document.getElementById('item').addEventListener('keyup', function (event) {
-    if (event.key === 'Enter'){
+    if (event.key === 'Enter') {
         document.querySelector('button').click();
     }
-    
-    });
 
-
-    
-function createNewListItem (itemName){
- 
-    let li = document.createElement('li');
-  
-  let spn = document.createElement('span');
-  spn.innerText = itemName;
-
-  let space = document.createTextNode('  ');
-  
-  
-  let btn = document.createElement('button');
-  btn.innerText = 'Delete';
-//Listener for delete button; 
-  btn.addEventListener('click', function () {
-    li.remove();
 });
+
+
+
+function createNewListItem(itemName) {
+
+    let li = document.createElement('li');
+
+    let spn = document.createElement('span');
+    spn.innerText = itemName;
+
+    let space = document.createTextNode('  ');
+
+
+    let btn = document.createElement('button');
+    btn.innerText = 'Delete';
+    btn.classList.add('red');
+    //Listener for delete button; 
+    btn.addEventListener('click', function () {
+        li.remove();
+    });
 
     li.appendChild(spn);
     li.appendChild(space);
     li.appendChild(btn);
     return li;
-    
+
 }
